cc54c803d81cb9de4f988d2bbb376994ecf1dcdd
fennelouski <nathanfennel@gmail.com>
2026-03-22 08:15:06 -0600

    Initial Commit

---
+++ DataMirrorTests/DataMirrorTests.swift
@@ -0,0 +1,17 @@
+//
+//  DataMirrorTests.swift
+//  DataMirrorTests
+//
+//  Created by Nathan Fennel on 3/22/26.
+//
+
+import Testing
+@testable import DataMirror
+
+struct DataMirrorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
